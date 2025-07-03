30556fa912f8f2027974ad8d5346c41f5f4f47c5
Gabs <smecrowl9@gmail.com>
2025-07-03 13:02:47 -0300

    MongoDB connection with repository and service

--- src/main/java/com/smecrow/mongodb/domain/User.java
+++ src/main/java/com/smecrow/mongodb/domain/User.java
@@ -1,6 +1,12 @@
 package com.smecrow.mongodb.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User {
+
+    @Id
     private String id;
     private String name;
     private String email;
