0c8620b35e774bd49c23d251b5dd0bb8c7056a89
Gabs <smecrowl9@gmail.com>
2025-07-04 17:41:35 -0300

    Add Post entity and repository

---
+++ src/main/java/com/smecrow/mongodb/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.smecrow.mongodb.repository;
+
+import com.smecrow.mongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
