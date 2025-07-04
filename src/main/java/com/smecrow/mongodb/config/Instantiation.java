c54aab5a69ee5191d1ebb4d50a1a04aa349bba72
Gabs <smecrowl9@gmail.com>
2025-07-04 17:46:44 -0300

    Refactor Post entity to use AuthorDTO instead of User

--- src/main/java/com/smecrow/mongodb/config/Instantiation.java
+++ src/main/java/com/smecrow/mongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.smecrow.mongodb.config;
 
 import com.smecrow.mongodb.domain.Post;
 import com.smecrow.mongodb.domain.User;
+import com.smecrow.mongodb.dto.AuthorDTO;
 import com.smecrow.mongodb.repository.PostRepository;
 import com.smecrow.mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +34,11 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Viagem para o Recife", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Viagem para o Recife", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
